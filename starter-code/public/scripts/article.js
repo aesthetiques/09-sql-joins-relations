ca98e9734807984cf70fb625fe8c6fa1c874ac50
Chris Closser <chrisclosser1@gmail.com>
2017-03-27 14:55:00 -0700

    fixed '='

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -50,7 +50,14 @@ Article.truncateTable = function(callback) {
 };
 
 Article.prototype.insertRecord = function(callback) {
-  $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
+  $.post('/articles', {
+    author: this.author,
+    authorUrl: this.authorUrl,
+    body: this.body,
+    category: this.category,
+    publishedOn: this.publishedOn,
+    title: this.title
+  })
   .then(function(data) {
     console.log(data);
     if (callback) callback();
